290cb5f464f5bd7e0430fa9bba7ef9601321931c
OleksandrDatso <47855764+OleksandrDatso@users.noreply.github.com>
2019-02-27 20:53:26 +0200

    Update Application_part_2.java

--- lesson-004_1/src/ua/lviv/lgs/homework/part2/Application_part_2.java
+++ lesson-004_1/src/ua/lviv/lgs/homework/part2/Application_part_2.java
@@ -5,8 +5,14 @@ public class Application_part_2 {
 		Car car1 = new Car(1500000, "Sport");
 		System.out.println(car1);
 		
-		Car car2 = new Car(2000000, "Luxury", "leather", "Standard", "Comfortable");
+		Car car2 = new Car(2000000, "Luxury", "leather", "Standard", "Comfortable", 12);
 		car2.setCost(3356000);
+		System.out.println(car2);
+		
+		car2.setWheelSize(45);
+		System.out.println("WheelSize = " + car2.getWheelSize());
+		
+		car2.setWheel("superWheel");
 		System.out.println(car2);
 	}
 }
